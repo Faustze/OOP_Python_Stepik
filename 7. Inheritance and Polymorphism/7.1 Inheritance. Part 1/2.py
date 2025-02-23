98f04b773003d5c51df9a2a2a5885997943bf57c
Artem Suvorov <baros24@mail.ru>
2025-02-23 16:59:24 +0300

    Add solutions for Module 7.1

---
+++ 7. Inheritance and Polymorphism/7.1 Inheritance. Part 1/2.py
@@ -0,0 +1,29 @@
+class Shape:
+    pass
+
+class Polygon(Shape):
+    pass
+
+class Circle(Shape):
+    pass
+
+class Quadrilateral(Polygon):
+    pass
+
+class Parallelogram(Quadrilateral):
+    pass
+
+class Rectangle(Parallelogram):
+    pass
+
+class Square(Rectangle):
+    pass
+
+class Triangle(Polygon):
+    pass
+
+class IsoscelesTriangle(Triangle):
+    pass
+
+class EquilateralTriangle(Triangle):
+    pass
