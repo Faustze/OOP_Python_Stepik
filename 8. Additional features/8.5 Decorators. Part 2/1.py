92ed2b1582267f5d03500c2030c6114e9a4234ff
Artem Suvorov <baros24@mail.ru>
2025-03-09 14:25:55 +0300

    Add solutions for 1, 2 tasks for Module 8.5

---
+++ 8. Additional features/8.5 Decorators. Part 2/1.py
@@ -0,0 +1,13 @@
+import functools
+
+def track_instances(cls):
+    cls.instances = []
+    old_init = cls.__init__
+
+    @functools.wraps(old_init)
+    def new_init(self, *args, **kwargs):
+        old_init(self, *args, **kwargs)
+        self.instances.append(self)
+        
+    cls.__init__ = new_init
+    return cls
